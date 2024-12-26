bd8383d467ca970e081e35c30329c5669322d88f
Choliem <c14230161@john.petra.ac.id>
2024-12-26 15:55:44 +0800

    Belajar Laravel 11 | 14. N + Sandika galih

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,12 +10,12 @@ class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
-
+    protected $with = ['author', 'category'];
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-    
+
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
